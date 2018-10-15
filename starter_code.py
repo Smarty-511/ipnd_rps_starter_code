03f61d01cb38b497db849fd0b9b91ca15abfe707
Ibrahim Al Shukri <alshukri511@gmail.com>
2018-10-15 22:34:16 +0400

    Add computer player option/choice

--- starter_code.py
+++ starter_code.py
@@ -4,7 +4,6 @@
 and reports both Player's scores each round."""
 
 moves = ['rock', 'paper', 'scissors']
-players = ['random', 'reflect', 'cycle', 'repeat']
 """The Player class is the parent class for all of the Players
 in this game"""
 
@@ -33,6 +32,9 @@ class HumanPlayer(Player):
                 x = input('Please choose a valid move: rock, paper, scissors, or z\n')
 
         return x.strip().lower()
+
+    def learn(self, my_move, their_move):
+        pass
 
 # This player will learn human's previous move and play it for the next round
 class ReflectPlayer(Player):
@@ -67,9 +69,9 @@ class CyclePlayer(Player):
             self.index +=1
         else:
             self.index = 0
-            self.cycle = moves[self.index]
-        return self.cycle
+        return moves[self.index]
 
+# returns 1 in case of human wins and 0 otherwise
 def beats(self, one, two):
     if one == two:
         return f"It's a Tie!\nHuman Score:{self.score1}     Computer Score: {self.score2}"
@@ -81,7 +83,6 @@ def beats(self, one, two):
     else: 
         self.score2 +=1
         return f"Computer wins!\nHuman Score:{self.score1}      Computer Score: {self.score2}"
-
 
 class Game:
 # score1 = human, score2 = computer
@@ -96,6 +97,7 @@ class Game:
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Human: {move1}  Computer: {move2}")
+        # beats to determine winner of round
         print(f"Result: {beats(self, move1,move2)}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
@@ -104,22 +106,26 @@ class Game:
         print("Game start!")
         for round in range(3):
             print(f"Round {round}:")
-            PC= self.rules()
-            return PC
             self.play_round()
         print("Game over!")
-    
-    def rules(self):
-        print('Game Rules:\nscissor cuts paper,\npaper covers rock,\nrock crushes scissors')
-        print('Choose one: "rock" --  "paper" -- "scissors"')
-        print('to quit, press z')
-        PC = input('Choose Computer Player!\nEnter "random", "reflect", "repeat", or "cycle"\n')
-        while PC not in players:
-            if PC != "z":
-                print ("Please enter a valid input")
-        return PC.strip().lower()
 
 
 if __name__ == '__main__':
-    game = Game(HumanPlayer(), CyclePlayer())
+    print('Game Rules:\nscissor cuts paper,\npaper covers rock,\nrock crushes scissors')
+    print('Choose one: "rock" --  "paper" -- "scissors"')
+    print('to quit, press z')
+    x = input('Choose Computer Player!\nEnter "random", "reflect", "repeat", or "cycle"\n')
+    while x not in ['random', 'reflect', 'cycle', 'repeat']:
+        if x != "z":
+            x = input("Please enter a valid input\n") 
+    if x == "random":
+        game = Game(HumanPlayer(), RandomPlayer())
+    elif x == "cycle":
+        game = Game(HumanPlayer(), CyclePlayer())
+    elif x == "repeat":
+        game = Game(HumanPlayer(), Player())
+    elif x == "reflect":
+        game = Game(HumanPlayer(), ReflectPlayer())             
+
+
     game.play_game()
